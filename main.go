97abec72e5cb4f818e7aedfcc41dabb971be8960
Alexey B <bas95work@gmail.com>
2024-07-18 00:55:10 +0300

    finish hw

--- main.go
+++ main.go
@@ -1,19 +1,143 @@
 package main
 
-func EvalSequence(matrix [][]int, userAnswer []int) int {
-	// validation
+import (
+	"errors"
+)
+
+// GraphLists граф в виде списков смежности
+type GraphLists struct {
+	adjList map[int][]int
+	weights map[int]map[int]int
+}
+
+func EvalSequence(matrix [][]int, userAnswer []int) (int, error) {
+	if !validateMatrix(matrix) {
+		return 0, errors.New("invalid matrix")
+	}
+
+	if !validateUserAnswer(matrix, userAnswer) {
+		return 0, errors.New("invalid user answer")
+	}
+
 	maxGrade := calMaxGrade(matrix)
 	userGrade := calcUserGrade(matrix, userAnswer)
 
-	percent := userGrade * 100 / maxGrade
+	if maxGrade == 0 {
+		return 0, nil
+	}
 
-	return percent
+	percent := (userGrade * 100) / maxGrade
+
+	return percent, nil
+}
+
+func validateMatrix(matrix [][]int) bool {
+	n := len(matrix)
+
+	for i := 0; i < n; i++ {
+		// матрица должна быть квадратной - количество строк должно совпадать с количеством столбцов
+		if len(matrix[i]) != n {
+			return false
+		}
+
+		// в графе не может быть петель - значения на главной диагонали должны быть равны нулю
+		if matrix[i][i] != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func validateUserAnswer(matrix [][]int, userAnswer []int) bool {
+	seen := make(map[int]bool)
+
+	for _, v := range userAnswer {
+		// ответы пользователя не должны выходить за диапазон матрицы
+		if v < 0 || v >= len(matrix) {
+			return false
+		}
+
+		// элементы в слайсе ответов пользователя должны быть уникальными
+		if seen[v] {
+			return false
+		}
+
+		seen[v] = true
+	}
+
+	return true
 }
 
 func calMaxGrade(matrix [][]int) int {
-	return -1
+	graph := matrixToGraph(matrix)
+	maxGrade := 0
+
+	for vertex := range graph.adjList {
+		visited := make(map[int]bool)
+
+		grade := dfs(vertex, visited, graph, 0)
+
+		if grade > maxGrade {
+			maxGrade = grade
+		}
+	}
+
+	return maxGrade
 }
 
 func calcUserGrade(matrix [][]int, userAnswer []int) int {
-	return 0
+	userGrade := 0
+
+	for i := 0; i < len(userAnswer)-1; i++ {
+		userGrade += matrix[userAnswer[i]][userAnswer[i+1]]
+	}
+
+	return userGrade
+}
+
+func matrixToGraph(matrix [][]int) GraphLists {
+	// adjList: Список смежности для хранения соседей каждой вершины
+	// weights: Мапа для хранения весов ребер. Представлена как map[int]map[int]int, где ключи - это вершины, а значения - это карты с весами ребер от вершины к ее соседям
+	graph := GraphLists{
+		adjList: make(map[int][]int),
+		weights: make(map[int]map[int]int),
+	}
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] != 0 {
+				if graph.weights[i] == nil {
+					graph.weights[i] = make(map[int]int)
+				}
+
+				graph.adjList[i] = append(graph.adjList[i], j)
+				graph.weights[i][j] = matrix[i][j]
+			}
+		}
+	}
+
+	return graph
+}
+
+// Функция выполняет поиск в глубину для нахождения пути с максимальной суммой весов в графе
+func dfs(node int, visited map[int]bool, graph GraphLists, currentGrade int) int {
+	visited[node] = true
+
+	maxGrade := currentGrade
+
+	for _, neighbor := range graph.adjList[node] {
+		if !visited[neighbor] {
+			grade := dfs(neighbor, visited, graph, currentGrade+graph.weights[node][neighbor])
+
+			if grade > maxGrade {
+				maxGrade = grade
+			}
+		}
+	}
+
+	// Снимаем отметку о посещении для текущей вершины, чтобы она могла быть использована в других путях
+	visited[node] = false
+
+	return maxGrade
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEvalSequence(t *testing.T) {
@@ -19,40 +20,98 @@ func TestEvalSequence(t *testing.T) {
 		{0, 1, 0, 0, 0},
 	}
 
+	mtx2 := [][]int{
+		{0, 1, 0, 0, 0, 0, 0},
+		{1, 0, 1, 1, 1, 0, 0},
+		{0, 1, 0, 0, 0, 1, 1},
+		{0, 1, 0, 0, 0, 1, 0},
+		{0, 1, 0, 0, 0, 0, 1},
+		{0, 0, 1, 1, 0, 0, 0},
+		{0, 0, 1, 0, 1, 0, 0},
+	}
+
 	tests := []struct {
-		name string
-		args args
-		want int
+		name      string
+		args      args
+		want      int
+		expectErr string
 	}{
 		{
-			name: "mtx 5 verticals 100%",
+			name: "mtx 5 vertices 100%",
 			args: args{
 				mtx: mtx1,
 				ua:  []int{4, 1, 0, 2},
 			},
-			want: 100,
+			want:      100,
+			expectErr: "",
 		},
 		{
-			name: "mtx 5 verticals 0%",
+			name: "mtx 5 vertices 0%",
 			args: args{
 				mtx: mtx1,
 				ua:  []int{},
 			},
-			want: 0,
+			want:      0,
+			expectErr: "",
 		},
 		{
-			name: "mtx 5 verticals 50%",
+			name: "mtx 5 vertices 50%",
 			args: args{
 				mtx: mtx1,
 				ua:  []int{4, 1, 0},
 			},
-			want: 50,
+			want:      50,
+			expectErr: "",
+		},
+		{
+			name: "mtx 7 vertices 100%",
+			args: args{
+				mtx: mtx2,
+				ua:  []int{0, 1, 3, 5, 2, 6, 4},
+			},
+			want:      100,
+			expectErr: "",
+		},
+		{
+			name: "mtx 7 vertices 83%",
+			args: args{
+				mtx: mtx2,
+				ua:  []int{0, 1, 3, 5, 2, 6},
+			},
+			want:      83,
+			expectErr: "",
+		},
+		{
+			name: "invalid matrix",
+			args: args{
+				mtx: [][]int{
+					{0, 1},
+					{1, 0, 0},
+				},
+				ua: []int{0, 1},
+			},
+			want:      0,
+			expectErr: "invalid matrix",
+		},
+		{
+			name: "invalid user answer",
+			args: args{
+				mtx: mtx1,
+				ua:  []int{0, 5},
+			},
+			want:      0,
+			expectErr: "invalid user answer",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := EvalSequence(tt.args.mtx, tt.args.ua)
-			assert.Equal(t, tt.want, got)
+			got, err := EvalSequence(tt.args.mtx, tt.args.ua)
+			if tt.expectErr != "" {
+				assert.EqualError(t, err, tt.expectErr)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
 		})
 	}
 }
